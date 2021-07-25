3fcc87151298bd93e951174fb749383063cb4741
rikaz rizlan <rikzrikaz@gmail.com>
2021-07-25 21:40:10 +0530

    bucket list collection

--- src/components/BucketList.js
+++ src/components/BucketList.js
@@ -1,39 +1,57 @@
 import { Button } from '@material-ui/core';
 import { ArrowBack } from '@material-ui/icons';
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import firebase from 'firebase/app';
 import { storage, db } from '../firebase';
 import { useAuth } from '../contexts/AuthContext';
 import '../App.css';
 
 function BucketList({ setOpen, open }) {
-    const [caption, setCaption] = useState("");
-    const bucketRef = useRef();
-    const [image, setImage] = useState(null);
+    const [bucketList, setBucketList] = useState("");
+    const [listItems, setListItems] = useState({});
     const [progress, setProgress] = useState(0);
-    const {currentUser} = useAuth();
+    const { currentUser } = useAuth();
+
+    useEffect(() => {
+        db.collection('users').doc(currentUser.uid).collection('bucketList').get().then((onSnapshot) => {
+            onSnapshot.forEach((doc) => {
+                console.log(doc.data());
+                setListItems(doc.data())
+            });
+        })
+    }, []);
 
     const handleUpload = () => {
         console.log("hello world");
+        db.collection('users').doc(currentUser?.uid).collection('bucketList').add({
+            timeStamp: firebase.firestore.FieldValue.serverTimestamp(),
+            listItem: bucketList,
+            username: currentUser?.displayName,
+        });
+        setProgress(0);
+        setBucketList("");
     }
+    console.log(listItems);
 
 
     return (
-        <div className={open ? "bucket-list-main":"bucket-list-hide"}>
+        <div className={open ? "bucket-list-main" : "bucket-list-hide"}>
             <ArrowBack onClick={() => setOpen(false)} />
             <center>Where to next?</center>
             <form className="post-form">
                 <div className="form-group">
-                    <input ref={bucketRef} onChange={event => setCaption(event.target.value)} value={caption} />
+                    <input onChange={event => setBucketList(event.target.value)} value={bucketList} />
                 </div>
                 <div className="btn-container">
                     <Button onClick={() => setOpen(false)} variant="contained" color="primary">Cancel</Button>
                     <Button className="btn-2" variant="contained" color="secondary" onClick={handleUpload}>+</Button>
                 </div>
             </form>
-            <ul>
-
-            </ul>
+            {/* <ul>
+                {
+                    listItems
+                }
+            </ul> */}
         </div>
     )
 }
