b2994062a0ef30d199c0e11bdab09c00078a1fe0
rikaz rizlan <rikzrikaz@gmail.com>
2021-08-20 21:56:49 +0530

    removed .

--- src/pages/App.js
+++ src/pages/App.js
@@ -149,7 +149,7 @@ function App() {
       <section className="banner" id="banner">
           <div className="content"> 
             <div className="info">
-              <h2>Get connected with fellow travellers.</h2>
+              <h2>Get connected with fellow travellers</h2>
               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id alias optio pariatur, quia laudantium sapiente nam quaerat illo veritatis voluptates itaque cupiditate ipsum exercitationem consectetur, vel qui iste provident placeat.</p>
               <Button variant="contained" color="secondary" onClick={() => setOpen(true)}>Get Started</Button>
             </div>
